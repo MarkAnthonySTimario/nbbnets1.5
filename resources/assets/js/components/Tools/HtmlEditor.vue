35b0dc0943f138db0e6693bbef10220a89bd995e
Kevin <contact.kevs@gmail.com>
2018-09-11 14:21:47 +0800

    removed froala

--- resources/assets/js/components/Tools/HtmlEditor.vue
+++ resources/assets/js/components/Tools/HtmlEditor.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-        <froala :tag="'textarea'" :config="config" v-model="raw"></froala>
+        <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"> -->
+        <!-- <froala :tag="'textarea'" :config="config" v-model="raw"></froala> -->
+        <textarea cols="30" rows="10" class="form-control input-sm" v-model="raw"></textarea>
     </div>
 </template>
 
